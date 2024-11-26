82bdc06ab76683723e808c6fc69a66c945fedfc0
Johnson M <johnson277600@gmail.com>
2024-11-26 10:01:17 +0530

    update

--- reactm/src/Car.js
+++ reactm/src/Car.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 // class Car extends React.Component{
 //   constructor(props){
 //     super();
@@ -121,82 +121,13 @@ import ReactDOM from "react-dom/client";
 
 //Components in Files
 
-// import React from "react";
+//  import React from "react";
 // import ReactDOM from 'react-dom/client';
-// import Car from './Car.js';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<Car />);
-
-// export default Car;
-
-// class Car extends React.Component{
-//     constructor(){
-//         super();
-//         this.state = {color:"red"};
-//     }
-//     render(){
-//         return <h2>I am a Car!</h2>;
-//     }
-// }
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<Car />);
-
-//Using State Object
-
-// class Car extends React.Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             brand: "Ford",
-//             model: "Mustang",
-//             color: "red",
-//             year: 1964
-//         };
-//     }
-//     render(){
-//         return (
-//             <div>
-//                 <h1>My {this.state.brand}</h1>
-//                 <p>It is  a {this.state.color}  
-//                      {this.state.model} 
-//                     from {this.state.year}.
-//                 </p>
-//             </div>
-//         );
-//     }
-// }
-
-// const container = document.getElementById('root');
-// const root = ReactDOM.createRoot(container);
-// root.render(<Car />);
-
-//Changing the state Object
-
-class Car extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            brand: "Ford",
-            model: "Mustang",
-            color: "red",
-            year: 1964
-        };
-    }
-    render(){
-        return (
-            <div>
-                <h1>My {this.state.brand}</h1>
-                <p>It is  a {this.state.color}  
-                     {this.state.model} 
-                    from {this.state.year}.
-                </p>
-            </div>
-        );
-    }
+function Car(){
+  return<h2>Hi I am a Car!</h2>
 }
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(<Car />);
+
+
+export default Car;
